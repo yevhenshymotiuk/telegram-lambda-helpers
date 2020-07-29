b469476e28a19b40833024056ff17d21d62dd832
Yevhen Shymotyuk <fly1ngdream@pm.me>
2020-07-29 20:19:49 +0300

    Return closure from SetWebhook function

--- webhook.go
+++ webhook.go
@@ -11,52 +11,55 @@ import (
 	"github.com/yevhenshymotiuk/telegram-lambda-helpers/apigateway"
 )
 
-// SetWebhook sets Telegram webhook to get updates
+// SetWebhook provides function which sets Telegram webhook to get updates
 func SetWebhook(
-	request events.APIGatewayProxyRequest,
 	telegramToken string,
-) (apigateway.Response, error) {
-	responseFail := apigateway.Response{StatusCode: 404}
+) func(request events.APIGatewayProxyRequest) (apigateway.Response, error) {
+	return func(request events.APIGatewayProxyRequest) (apigateway.Response, error) {
+		responseFail := apigateway.Response{StatusCode: 404}
 
-	bot, err := tgbotapi.NewBotAPI(telegramToken)
-	if err != nil {
-		return responseFail, err
+		bot, err := tgbotapi.NewBotAPI(telegramToken)
+		if err != nil {
+			return responseFail, err
+		}
+
+		url := fmt.Sprintf(
+			"https://%s/%s/",
+			request.Headers["Host"],
+			request.RequestContext.Stage,
+		)
+		_, err = bot.SetWebhook(tgbotapi.NewWebhook(url))
+		if err != nil {
+			return responseFail, err
+		}
+
+		info, err := bot.GetWebhookInfo()
+		if err != nil {
+			return responseFail, err
+		}
+		if info.LastErrorDate != 0 {
+			log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+		}
+
+		var buf bytes.Buffer
+
+		body, err := json.Marshal(
+			map[string]interface{}{"message": "Webhook was successfully set!"},
+		)
+		if err != nil {
+			return responseFail, err
+		}
+		json.HTMLEscape(&buf, body)
+
+		resp := apigateway.Response{
+			StatusCode:      200,
+			IsBase64Encoded: false,
+			Body:            buf.String(),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+
+		return resp, nil
 	}
-
-	url := fmt.Sprintf(
-		"https://%s/%s/",
-		request.Headers["Host"],
-		request.RequestContext.Stage,
-	)
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(url))
-	if err != nil {
-		return responseFail, err
-	}
-
-	info, err := bot.GetWebhookInfo()
-	if err != nil {
-		return responseFail, err
-	}
-	if info.LastErrorDate != 0 {
-		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
-	}
-
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(
-		map[string]interface{}{"message": "Webhook was successfully set!"},
-	)
-	if err != nil {
-		return responseFail, err
-	}
-	json.HTMLEscape(&buf, body)
-
-	resp := apigateway.Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers:         map[string]string{"Content-Type": "application/json"},
-	}
-
-	return resp, nil
 }
