fc5527bc84de3e446a747bc22cf4cf962e93ecfe
Yevhen Shymotyuk <fly1ngdream@pm.me>
2020-08-02 16:28:06 +0300

    Add exported variables for Response objects with different status codes

--- apigateway/apigateway.go
+++ apigateway/apigateway.go
@@ -5,3 +5,14 @@ import "github.com/aws/aws-lambda-go/events"
 
 // Response is an alias for APIGatewayProxyResponse
 type Response events.APIGatewayProxyResponse
+
+var (
+	// Response200 is Response object with status code 200
+	Response200 = Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            "Ok",
+	}
+	// Response404 is Response object with status code 404
+	Response404 = Response{StatusCode: 404}
+)
